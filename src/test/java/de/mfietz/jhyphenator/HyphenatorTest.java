a4433649ecbc5f58314d173d4be861b5521e0f05
nexos <nexoscp@users.noreply.github.com>
2020-05-22 17:54:40 +0200

    update JUnit to 5.6.0

--- src/test/java/de/mfietz/jhyphenator/HyphenatorTest.java
+++ src/test/java/de/mfietz/jhyphenator/HyphenatorTest.java
@@ -1,59 +1,45 @@
 package de.mfietz.jhyphenator;
 
-import junitparams.JUnitParamsRunner;
-import junitparams.Parameters;
-import org.junit.Ignore;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import javax.annotation.Nonnull;
+
+import java.util.stream.Stream;
+
+import static de.mfietz.jhyphenator.HyphenationPattern.*;
+import static de.mfietz.jhyphenator.Hyphenator.getInstance;
 import static java.lang.String.join;
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@RunWith(JUnitParamsRunner.class)
 public class HyphenatorTest {
-
-    @Test
-    @Parameters({
-      "Kochschule, Koch-schu-le", 
-      "Seewetterdienst, See-wet-ter-dienst",
-      "Hochverrat, Hoch-ver-rat", 
-      "Musterbeispiel, Mus-ter-bei-spiel",
-      "Bundespräsident, Bun-des-prä-si-dent", 
-      "Schmetterling, Schmet-ter-ling",
-      "Christian, Chris-ti-an"
-    }) 
-    public void testDe(String input, String expected) {
-        HyphenationPattern de = HyphenationPattern.lookup("de");
-        Hyphenator h = Hyphenator.getInstance(de);
-        String actual = join("-", h.hyphenate(input));
-        assertEquals(expected, actual);
+    private static Stream<Arguments> data() {
+        return Stream.of(
+                Arguments.of(DE, "Kochschule", "Koch-schu-le"),
+                Arguments.of(DE, "Seewetterdienst", "See-wet-ter-dienst"),
+                Arguments.of(DE, "Hochverrat", "Hoch-ver-rat"),
+                Arguments.of(DE, "Musterbeispiel", "Mus-ter-bei-spiel"),
+                Arguments.of(DE, "Bundespräsident", "Bun-des-prä-si-dent"),
+                Arguments.of(DE, "Schmetterling", "Schmet-ter-ling"),
+                Arguments.of(DE, "Christian", "Chris-ti-an"),
+                Arguments.of(EN_US, "crocodile", "croc-o-dile"),
+                Arguments.of(EN_US, "activity", "ac-tiv-ity"),
+                //Arguments.of(EN_US, "potato", "po-ta-to"), FIXME
+                //Arguments.of(EN_US, "hyphenation", "hy-phen-a-tion"), FIXME
+                Arguments.of(EN_US, "podcast", "pod-cast"),
+                Arguments.of(EN_US, "message", "mes-sage")
+                //Arguments.of(HU, "segítség", "se-gít-ség")
+        );
     }
 
-    @Test
-    @Parameters({
-      "crocodile, croc-o-dile", 
-      "activity, ac-tiv-ity",
- //     "potato, po-ta-to", FIXME
- //     "hyphenation, hy-phen-a-tion", FIXME
-      "podcast, pod-cast",
-      "message, mes-sage"
-    })
-    public void testEnUs(String input, String expected) {
-        HyphenationPattern us = HyphenationPattern.lookup("en_us");
-        Hyphenator h = Hyphenator.getInstance(us);
-        String actual = join("-", h.hyphenate(input));
+    @ParameterizedTest
+    @MethodSource("data")
+    public void testDe(
+            final @Nonnull HyphenationPattern pattern,
+            final @Nonnull String input,
+            final @Nonnull String expected) {
+        final var actual = join("-", getInstance(pattern).hyphenate(input));
         assertEquals(expected, actual);
     }
-    
-    @Test
-    @Parameters({
-      "segítség, se-gít-ség" 
-    })
-    @Ignore ("no data for hu")
-    public void testHu(String input, String expected) {
-      HyphenationPattern us = HyphenationPattern.lookup("hu");
-      Hyphenator h = Hyphenator.getInstance(us);
-      String actual = join("-", h.hyphenate(input));
-      assertEquals(expected, actual);
-  }
 }
